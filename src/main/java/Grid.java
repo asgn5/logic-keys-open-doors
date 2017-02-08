75e33ab72c055ae3e8f251915abc2608e9646d18
asgn5 <r.cantware@gmail.com>
2017-02-07 19:42:55 -0500

    test

--- src/main/java/Grid.java
+++ src/main/java/Grid.java
@@ -19,11 +19,12 @@ public class Grid {
     public void init() {
         for (int i = 0; i < x; i++) {
             c[i] = new Category(""+i, y);
-	    for (int j = 0; j < 1; j++) {
-		for (int k = 0; k < x - 1; k++)
-		    m[k] = new Matrix(y, c[0], c[k+1]);
-		for (int l = x-1; l > 1; l--)
-		    m[l] = new Matrix(y, c[l],c[l-1]);
+            for (int j = 0; j < 1; j++) {
+                for (int k = 0; k < x - 1; k++)
+                    m[k] = new Matrix(y, c[0], c[k + 1]);
+                for (int l = x - 1; l > 1; l--)
+                    m[l] = new Matrix(y, c[l], c[l - 1]);
+            }
 	    }
     }
 
